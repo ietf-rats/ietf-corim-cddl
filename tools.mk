19941244fb65222b3bf2fc93922fab462909abdb
Henk Birkholz <henk.birkholz@sit.fraunhofer.de>
2021-04-01 18:56:23 +0200

    expand embedded CBOR

--- tools.mk
+++ tools.mk
@@ -10,3 +10,9 @@ curl ?= $(shell command -v curl)
 ifeq ($(strip $(curl)),)
 $(error curl not found)
 endif
+
+diag2diag ?= $(shell command -v diag2diag.rb)
+ifeq ($(strip $(diag2diag)),)
+$(error diag2diag.rb not found. To install diag2diag.rb: 'gem install cbor-diag')
+endif
+
